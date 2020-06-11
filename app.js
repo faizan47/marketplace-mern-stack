61b91eb151a19973fb186e0143c0a61b2fd5eb33
faizan <fzn9898@gmail.com>
2020-06-12 02:37:40 +0530

    handle error mongodb connection

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const mongoose = require('mongoose');
 const { mongoURI } = require('./config/keys');
 require('./models/User');
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-	console.log('Successfully connected to the database')
-);
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Successfully connected to the database');
+	}
+});
 app.get('/', (req, res) => res.send('It works!'));
 
 app.listen(port, () => console.log('Listening on port 5000'));
--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
 	mongoURI:
-		'mongodb://uietgi4wd3glbcp4rmg9:nrBWZXK9NQKWdf1VGGdL@b9p2ijy8n1qgnqj-mongodb.services.clever-cloud.com:27017/b9p2ijy8n1qgnqj'
+		'mongodb://uietgi4wd3glbcp4rmg9:fsJf7fu09AOHLwqO1M2W@b9p2ijy8n1qgnqj-mongodb.services.clever-cloud.com:27017/b9p2ijy8n1qgnqj'
 };
