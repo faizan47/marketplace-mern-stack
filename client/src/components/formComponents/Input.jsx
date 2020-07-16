b678b1dcc28759b1ce4bcca718cd89805810ce77
faizan <fzn9898@gmail.com>
2020-07-16 16:15:30 +0530

    improve input markup logic for number field

--- client/src/components/formComponents/Input.jsx
+++ client/src/components/formComponents/Input.jsx
@@ -3,13 +3,19 @@ import React, { Fragment } from 'react';
 export default props => {
 	const { placeholder, iconClass, label, input, meta } = props;
 	const { touched, error } = meta;
+	const inputMarkup =
+		input.type === 'number' ? (
+			<input {...input} type="text" className="input" placeholder={placeholder} />
+		) : (
+			<input {...input} className="input" placeholder={placeholder} />
+		);
 
 	return (
 		<Fragment>
 			<div className="field">
 				{label ? <label className="label">{label}</label> : null}
 				<div className="control has-icons-left">
-					<input {...input} type="text" className="input" placeholder={placeholder} />
+					{inputMarkup}
 					<span className="icon is-small is-left">
 						<i className={`fas ${iconClass}`} />
 					</span>
